773509d25931748ea69a5e6a46a790ce2821c023
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2025-02-12 11:20:16 -0300

    add parameter handling and validation for JWT processing

--- lib/resty/libjwt.lua
+++ lib/resty/libjwt.lua
@@ -1,7 +1,35 @@
 local _M = {}
 
-function _M.validate()
-    return false
+function _M.get_params(params)
+    local result = {
+        header_token = "Authorization",
+    }
+    if params == nil then
+        return nil, "params is required"
+    end
+
+    if params["header_token"] ~= nil then
+        result.header_token = params["header_token"]
+    end
+    if params["jwks_files"] == nil then
+        return nil, "jwks_files is required"
+    end
+    if type(params["jwks_files"]) ~= "table" then
+        return nil, "jwks_files is not an array"
+    end
+    result.jwks_files = params["jwks_files"]
+    return result, ""
 end
+
+function _M.validate(params)
+    local params, err = _M.get_params(params)
+    if err ~= "" then
+        return nil, err
+    end
+    return false, "Not implemented"
+end
+
+
+
 
 return _M
