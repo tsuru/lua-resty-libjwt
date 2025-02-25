f3bc9bf7e19cd8fa6f2e474f074c8b1eb3faad42
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2025-02-25 14:51:35 -0300

    refactor: clean up spacing in token test assertions

--- test/get_token_test.lua
+++ test/get_token_test.lua
@@ -7,14 +7,14 @@ function TestShouldReturnErrorWhenNotFindingTheToken()
     }
     local result, err = utils.get_token(headers, "header89")
     lu.assertEquals(err, "token not found")
-    lu.assertEquals(result, nil )
+    lu.assertEquals(result, nil)
 
     local headers = {
         ["header_token"] = "token123",
     }
     local result, err = utils.get_token(headers, "header_token")
     lu.assertEquals(err, "token not found")
-    lu.assertEquals(result, nil )
+    lu.assertEquals(result, nil)
 end
 
 function TestShouldNotReturnErrorWhenCorrectParameters()
@@ -23,8 +23,7 @@ function TestShouldNotReturnErrorWhenCorrectParameters()
     }
     local result, err = utils.get_token(headers, "token")
     lu.assertEquals(err, "")
-    lu.assertEquals(result, "123" )
+    lu.assertEquals(result, "123")
 end
 
-
-os.exit( lu.LuaUnit.run() )
+os.exit(lu.LuaUnit.run())
