3fb2a0df559a8b6cf7af44fdc0b25ab13697a8e1
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2025-02-27 09:43:30 -0300

    ci: update GitHub Actions workflow to separate unit and E2E tests

--- test/params_test.lua
+++ test/params_test.lua
@@ -4,20 +4,19 @@ local utils = require('../lib/resty/libjwt/utils')
 
 function TestShouldReturnUndefinedWhenParamsIsNil()
     local result, err = utils.get_params()
-    lu.assertEquals(err, "params is required" )
-    lu.assertEquals(result, nil )
+    lu.assertEquals(err, "params is required")
+    lu.assertEquals(result, nil)
 
     local result, err = utils.get_params(nil)
-    lu.assertEquals(err, "params is required" )
-    lu.assertEquals(result, nil )
+    lu.assertEquals(err, "params is required")
+    lu.assertEquals(result, nil)
 end
 
-
 function TestShouldReturnUndefinedWhenFilesIsNil()
-    local params = {["header_token"] = "token"}
+    local params = { ["header_token"] = "token" }
     local result, err = utils.get_params(params)
-    lu.assertEquals(err, "jwks_files is required" )
-    lu.assertEquals(result, nil )
+    lu.assertEquals(err, "jwks_files is required")
+    lu.assertEquals(result, nil)
 end
 
 function TestShouldReturnValuesWhenFilesIsNotNil()
@@ -26,21 +25,22 @@ function TestShouldReturnValuesWhenFilesIsNotNil()
         ["jwks_files"] = "files",
     }
     local result, err = utils.get_params(params)
-    lu.assertEquals(err, "jwks_files is not an array" )
-    lu.assertEquals(result, nil )
+    lu.assertEquals(err, "jwks_files is not an array")
+    lu.assertEquals(result, nil)
 end
 
 function TestShouldReturnValidatedParams()
     local params = {
         ["header_token"] = "token",
-        ["jwks_files"] = {"files"},
+        ["jwks_files"] = { "files" },
     }
     local result, err = utils.get_params(params)
-    lu.assertEquals(err, "" )
+    lu.assertEquals(err, "")
     lu.assertEquals(result, {
         header_token = "token",
-        jwks_files = {"files"},
+        jwks_files = { "files" },
+        return_unauthorized_default = true
     })
 end
 
-os.exit( lu.LuaUnit.run() )
+os.exit(lu.LuaUnit.run())
