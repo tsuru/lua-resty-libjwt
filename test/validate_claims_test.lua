6b365a6f62d496833837153f5ac4395b3df0295f
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2025-03-20 16:00:27 -0300

    test: add unit tests for claims validation logic

---
+++ test/validate_claims_test.lua
@@ -0,0 +1,69 @@
+local lu = require('luaunit')
+local utils = require('../lib/resty/libjwt/utils')
+
+
+function TestShouldReturnErrorPatternInvalid()
+    local params = {
+        iss = { exact = "tsuru" },
+        aud = { one_of = { "audience1", "audience2" } },
+        sub = { pattern = ".*@g%.globo" },
+    }
+    local claims = {
+        iss = "tsuru",
+        aud = "audience1",
+        sub = "tsuru.team",
+    }
+    local result, err
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "Claim 'sub' does not match required pattern")
+end
+
+function TestShouldReturnErrorOneOfInvalid()
+    local params = {
+        iss = { exact = "tsuru" },
+        aud = { one_of = { "audience1", "audience2" } },
+        sub = { pattern = ".*@g%.globo" },
+    }
+    local claims = {
+        iss = "tsuru",
+        aud = "audience3",
+        sub = "tsuru.team@g.globo",
+    }
+    local err
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "Claim 'aud' must be one of the allowed values")
+end
+
+function TestShouldReturnErrorExactInvalid()
+    local params = {
+        iss = { exact = "tsuru" },
+        aud = { one_of = { "audience1", "audience2" } },
+        sub = { pattern = ".*@g%.globo" },
+    }
+    local claims = {
+        iss = "tsuruu",
+        aud = "audience1",
+        sub = "tsuru.team@g.globo",
+    }
+    local err
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "Claim 'iss' must be exactly 'tsuru'")
+end
+
+function TestShouldReturnOkInValidation()
+    local params = {
+        iss = { exact = "tsuru" },
+        aud = { one_of = { "audience1", "audience2" } },
+        sub = { pattern = ".*@g%.globo" },
+    }
+    local claims = {
+        iss = "tsuru",
+        aud = "audience1",
+        sub = "tsuru.team@g.globo",
+    }
+    local err
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "")
+end
+
+os.exit(lu.LuaUnit.run())
