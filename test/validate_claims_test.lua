db4133ba91d11c6040984bb1104f79a9f29afc42
Wilson Júnior <wilsonpjunior@gmail.com>
2025-10-15 12:40:54 -0300

    Add support to multiple validate_claims constraints

--- test/validate_claims_test.lua
+++ test/validate_claims_test.lua
@@ -66,4 +66,40 @@ function TestShouldReturnOkInValidation()
     lu.assertEquals(err, "")
 end
 
+function TestShouldSupportArray()
+    local params = {
+        {
+            iss = {exact = "tsuru"},
+            aud = {one_of = {"audience1", "audience2"}},
+            sub = {pattern = ".*@g%.globo"}
+        }, {
+            iss = {exact = "kubernetes"},
+            aud = {one_of = {"audience3", "audience4"}},
+            sub = {pattern = ".*@corp%.globo%.com"}
+        }
+    }
+    local claims = {
+        iss = "tsuru",
+        aud = "audience1",
+        sub = "tsuru.team@g.globo"
+    }
+    local err
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "")
+
+    claims = {
+        iss = "kubernetes",
+        aud = "audience4",
+        sub = "kubernetes4.team@corp.globo.com"
+    }
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err, "")
+
+    claims = {iss = "tsuru", aud = "audience4", sub = "tsuru.team@g.globo"}
+    err = utils.validate_claims(params, claims)
+    lu.assertEquals(err,
+                    "validate_claims constraint number 1: Claim 'aud' must be one of the allowed values OR " ..
+                        "validate_claims constraint number 2: Claim 'iss' must be exactly 'kubernetes'")
+end
+
 os.exit(lu.LuaUnit.run())
